fee1965c498fd7693b22880c2f9f1300ab4e190f
Joseph Oladipupo <oladipupojoseph66@gmail.com>
2019-03-05 23:13:38 +0100

    changes

--- sale_anmietungen/models/sale.py
+++ sale_anmietungen/models/sale.py
@@ -18,9 +18,9 @@ class SaleOrder(models.Model):
 		if vals.get('sale_type') == 'rental':
 			self.env['x_mieter'].create({
 				'x_studio_buchungsnummer': res.id,
-				'x_name': res.name,
-				'x_studio_beginin_der_reise_1': res.x_studio_von,
-				'x_studio_ende_der_reise_1': res.x_studio_bis
+				'x_name': vals.get('name'),
+				'x_studio_beginin_der_reise_1': vals.get('x_studio_von'),
+				'x_studio_ende_der_reise_1': vals.get('x_studio_bis'),
 			})
 		return res
 		
@@ -31,24 +31,11 @@ class SaleOrder(models.Model):
 			if vals.get('sale_type') == 'sale':
 				x_m = self.env['x_mieter'].search([('x_studio_buchungsnummer','=', rec.id)])
 				x_m.unlink()
-				
-			if vals.get('x_studio_von') and rec.sale_type == 'rental':
+			elif rec.sale_type == 'rental':
 				x_m = self.env['x_mieter'].search([('x_studio_buchungsnummer','=', rec.id)])
 				if x_m:
 					x_m.write({
-						'x_studio_beginin_der_reise_1': rec.x_studio_von
-					})
-				else:
-					self.env['x_mieter'].create({
-						'x_studio_buchungsnummer': rec.id,
-						'x_name': rec.name,
 						'x_studio_beginin_der_reise_1': rec.x_studio_von,
-						'x_studio_ende_der_reise_1': rec.x_studio_bis
-					})
-			elif vals.get('x_studio_bis') and rec.sale_type == 'rental':
-				x_m = self.env['x_mieter'].search([('x_studio_buchungsnummer','=', rec.id)])
-				if x_m:
-					x_m.write({
 						'x_studio_ende_der_reise_1': rec.x_studio_bis
 					})
 				else:
