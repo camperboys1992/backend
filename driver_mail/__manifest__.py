496637a9152304cf2cfd8079f9382ce919130030
Joseph Oladipupo <oladipupojoseph66@gmail.com>
2019-03-14 11:09:57 +0100

    fix

---
+++ driver_mail/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Send Mail Driver Att.',
+    'category': '',
+    'author': "Onedoos",
+    'summary': "Mail sending",
+    'depends': [
+		'fleet', 'studio_customization'
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
---
+++ driver_mail/models/fleet_vehicle.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
+	
+class FleetVehicle(models.Model):
+	_inherit = 'fleet.vehicle.assignation.log'
+	
+	@api.multi
+	def send_att_mail(self):
+		for rec in self:
+			template = self.ref('studio_customization.handover_mail')
+			_logger.info('>>>>>>>>>>> %s', template)
+			if template:
+				attachment = self.env['ir.attachment'].create({
+					'datas_fname': 'Driver Log',
+					'name': 'Driver Log',
+					'datas': rec.x_studio_field_Ru46t
+				})
+				_logger.info('>>>>>>>>>>> %s', attachment)
+				
+				template.write({
+					'attachment_ids': [(6, 0, [attachment.id])],
+				})
+					
+				template.send_mail(rec.id, force_send=True)
