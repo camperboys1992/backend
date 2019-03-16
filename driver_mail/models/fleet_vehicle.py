dd4f1b98033ca621beb500c27bdcd7e525c9e5cb
Joseph Oladipupo <oladipupojoseph66@gmail.com>
2019-03-16 13:52:56 +0100

    clean up

--- driver_mail/models/fleet_vehicle.py
+++ driver_mail/models/fleet_vehicle.py
@@ -10,14 +10,12 @@ class FleetVehicle(models.Model):
 	def send_att_mail(self):
 		for rec in self:
 			template = self.env.ref('studio_customization.handover_mail')
-			_logger.info('>>>>>>>>>>> %s', template)
 			if template:
 				attachment = self.env['ir.attachment'].create({
 					'datas_fname': 'Driver Log.pdf',
 					'name': 'Driver Log',
 					'datas': rec.x_studio_field_Ru46t
 				})
-				_logger.info('>>>>>>>>>>> %s', attachment)
 				
 				template.write({
 					'attachment_ids': [(6, 0, [attachment.id])],
