efddda0e6723706e1d16001853146191c6c4e946
Tjones787 <oladipupojoseph66@gmail.com>
2019-03-24 20:41:27 +0100

    Update fleet_vehicle.py

--- driver_mail/models/fleet_vehicle.py
+++ driver_mail/models/fleet_vehicle.py
@@ -12,8 +12,8 @@ class FleetVehicle(models.Model):
 			template = self.env.ref('studio_customization.handover_mail')
 			if template:
 				attachment = self.env['ir.attachment'].create({
-					'datas_fname': 'Driver Log.pdf',
-					'name': 'Driver Log',
+					'datas_fname': 'Handover Protocol.pdf',
+					'name': 'Handover Protocol',
 					'datas': rec.x_studio_field_Ru46t
 				})
 				
